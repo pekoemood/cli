e2fad31a776377315109ff73ea7bfcbf767b65f6
Ryotarou Shiozawa <pekoemood@gmail.com>
2026-02-17 23:17:19 +0900

    Todoアプリの実装2日目

--- Todo/Domain/TaskList.php
+++ Todo/Domain/TaskList.php
@@ -2,7 +2,7 @@
 
 class TaskList {
   private array $tasks = [];
-  private int $nextId = 1;
+  private int $nextId = 0;
 
   public function addTask(string $name): void {
     $task = new Task($this->nextId, $name);
@@ -10,5 +10,13 @@ class TaskList {
     $this->tasks[] = $task;
   }
 
-
+  public function deleteTask(int $taskId): void {
+    foreach($this->tasks as $i => $task) {
+      if ($task->getId() === $taskId) {
+        array_splice($this->tasks, $i, 1);
+        return;
+      }
+    }
+    throw new RuntimeException('タスクが見つかりません');
+  }
 }
